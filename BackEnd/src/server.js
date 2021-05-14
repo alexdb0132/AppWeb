de46ef38cd26342fe41ec969d22ef6fed9154d7e
kevinbriand4 <kevinbriand4@gmail.com>
2021-05-14 14:28:49 -0400

    creation objet musique

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,3 +2,21 @@ import express from 'express';
 
 const app = express();
 
+const pieces = {
+    'Daddy': {
+      'artiste': "Charlotte Cardin",
+      'Categorie': "pop",
+    },
+    'Fever': {
+        'artiste': "The McCoys",
+        'Categorie': "rock",
+      },
+    'Peaches': {
+        'artiste': "Justin Bieber",
+        'Categorie': "pop",
+    },
+    'September': {
+        'artiste': "Earth, Wind & fire",
+        'Categorie': "Disco",
+        },
+};
