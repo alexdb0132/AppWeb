d77081605ea23932ba935b8011a93aafd821776a
Alexandre <alexdb0132@gmail.com>
2021-05-14 15:45:29 -0400

    Auto stash before merge of "main" and "origin/main"

---
+++ FrontEnd/exercice_musique/src/Pages/PageAccueil.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PageAccueil()
+{
+    return(
+        <>
+            <h1>Bienvenue!</h1>
+            <p>Banane au chocolat!</p>
+        </>
+    )
+}
+
+export default PageAccueil;
