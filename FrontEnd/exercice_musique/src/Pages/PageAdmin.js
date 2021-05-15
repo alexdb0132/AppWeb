6bd127885990f56d7bc7a87cb68de8bc285313ea
Alexandre <alexdb0132@gmail.com>
2021-05-15 15:07:19 -0400

    Commencer page admin

---
+++ FrontEnd/exercice_musique/src/Pages/PageAdmin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PageAdmin({Repertoire})
+{
+    const repertoire = Repertoire;
+
+    return(
+        <>
+            
+        </>
+    )
+}
+
+export default PageAdmin
