ddcf48f885cf483e7834d4a8d1e2d1a5affd107f
philiposAmigos <philant.daumas@gmail.com>
2021-05-15 15:41:47 -0400

    Modification de PageCategories.js

--- FrontEnd/exercice_musique/src/Pages/PageCategories.js
+++ FrontEnd/exercice_musique/src/Pages/PageCategories.js
@@ -5,6 +5,13 @@ import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Tab from 'react-bootstrap/Tab';
+
+const categories = [
+  "pop",
+  "rock",
+  "disco"
+];
 
 const pieces = [
     {
@@ -29,33 +36,57 @@ const pieces = [
     },
 ]
 
+const options = [
+  "ajouter",
+  "modifier",
+  "supprimer"
+];
+
 function PageCategories(){
-    return (
-        <>
-            <Row>
-                <h1>Ajouter</h1>
-                <Col><Button>Ajouter</Button></Col>
-                <Col><Button>Modifier</Button></Col>
-                <Col><Button>Supprimer</Button></Col>
-            </Row>
-        </>
-    );
+  return (
+    <>
+    <Tab.Container>
+    <Row>
+      <Col>
+        <ListGroup defaultActiveKey="" >
+          {
+            options.map(optionCourante => 
+                <ListGroup.Item action href={`#${optionCourante}`}>{optionCourante}</ListGroup.Item>
+            )
+          }
+        </ListGroup>
+      </Col>
+      <Col>
+        <Tab.Content>
+          {
+            options.map(optionCourante => 
+              <Tab.Pane eventKey={`#${optionCourante}`}>
+                <AfficherCategorie option={optionCourante}/>
+              </Tab.Pane>
+            )
+          }
+        </Tab.Content>
+      </Col>
+    </Row>
+    </Tab.Container>
+    </>
+  );
 }
 
-function AjouterCategorie(){
-    return (
-        <>
-            
-        </>
-    );
-}
-
-function ModifierCategorie(){
-//revoir doc bootstrap form.control select
-}
-
-function SupprimerCategorie(){
-//revoir doc bootstrap form.control select
+function AfficherCategorie({ option }){
+  return (
+    <>
+      <Form>
+        <Form.Group>
+        <Form.Label>{option} une catégorie</Form.Label>
+        <Form.Control size="small" type="text"/>
+        </Form.Group>
+      </Form>
+      <Button>
+          {option}
+      </Button>
+    </>
+  );
 }
 
 export default PageCategories;
