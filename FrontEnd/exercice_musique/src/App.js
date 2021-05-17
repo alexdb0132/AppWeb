e6576c99230490bd8bacd18477aed2b79dbf763c
kevin briand <kevinbriand4@gmail.com>
2021-05-17 13:31:24 -0400

    ajouter post

--- FrontEnd/exercice_musique/src/App.js
+++ FrontEnd/exercice_musique/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, {useState} from 'react';
 import {
   BrowserRouter as Router,
@@ -54,3 +55,59 @@ function App() {
 }
 
 export default App;
+=======
+import React, {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Route 
+} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+
+import './App.css';
+
+import BarreNavigation from './Navigation/BarreNavigation';
+import PageAccueil from './Pages/PageAccueil';
+import PageCategories from './Pages/PageCategories';
+import PageModifierPiece from './Pages/PageModifierPiece';
+
+const pieces = [
+  {
+    titre: 'Daddy',
+    artiste: 'Charlotte Cardin',
+    categorie: 'pop'
+  },
+  {
+    titre: 'Fever',
+    artiste: 'The McCoys',
+    categorie: 'rock'
+  },
+  {
+    titre: 'Peaches',
+    artiste: 'Jusint Bieber',
+    categorie: 'pop'
+  },
+  {
+    titre: 'September',
+    artiste: 'Earth, Wind, Fire',
+    categorie: 'Disco'
+  },
+]
+
+function App() {
+
+  return (
+    <Router>
+      <Container>
+        <BarreNavigation/>
+        <Route path="/" component={PageAccueil} exact/>
+        <Route path="/categories" component={PageCategories} />
+        <Route path="/modifier/:id" component={PageModifierPiece} />
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
+>>>>>>> Stashed changes
