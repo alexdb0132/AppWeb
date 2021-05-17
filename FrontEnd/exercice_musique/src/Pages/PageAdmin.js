cfea8be173e1f1bd876c83764057d6c9e81d01a0
Alexandre <alexdb0132@gmail.com>
2021-05-17 15:45:19 -0400

    bouton modifier envoi a bonne place

--- FrontEnd/exercice_musique/src/Pages/PageAdmin.js
+++ FrontEnd/exercice_musique/src/Pages/PageAdmin.js
@@ -21,17 +21,10 @@ function PageAdmin()
     return(
         <>
             <Table striped bordered hover>
-<<<<<<< Updated upstream
-                {repertoire.map(piece => <tr><td>Titre: {piece.titre}, Artiste: {piece.artiste}, Categorie: {piece.categorie} <br/>
-                <Button size="sm" active="true" variant="primary">Modifier</Button>
-                <nbsp/>
-                <Button size="sm" active="true" variant="primary" component={Link} to="/supprimer/Daddy">Supprimer</Button></td></tr>)}
-=======
                 {repertoire.map((piece, index) => <tr><td>Titre: {piece.titre}, Artiste: {piece.artiste}, Categorie: {piece.categorie} <br/>
                 <Link key={index} to={`/modifier/${index}`}><Button size="sm" active="true" variant="primary" >Modifier</Button></Link>
                 <nbsp/>
                 <Button size="sm" active="true" variant="primary" >Supprimer</Button></td></tr>)}
->>>>>>> Stashed changes
             </Table>
         </>
     );
