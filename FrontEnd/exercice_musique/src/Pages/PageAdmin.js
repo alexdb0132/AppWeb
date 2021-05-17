19740f6d2017f63e75d5264b5eea0302588c80ba
Alexandre <alexdb0132@gmail.com>
2021-05-17 15:43:51 -0400

    Auto stash before merge of "main" and "origin/main"

--- FrontEnd/exercice_musique/src/Pages/PageAdmin.js
+++ FrontEnd/exercice_musique/src/Pages/PageAdmin.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from 'react-router-dom';
 
 function PageAdmin()
 {
@@ -20,10 +21,17 @@ function PageAdmin()
     return(
         <>
             <Table striped bordered hover>
+<<<<<<< Updated upstream
                 {repertoire.map(piece => <tr><td>Titre: {piece.titre}, Artiste: {piece.artiste}, Categorie: {piece.categorie} <br/>
                 <Button size="sm" active="true" variant="primary">Modifier</Button>
                 <nbsp/>
                 <Button size="sm" active="true" variant="primary" component={Link} to="/supprimer/Daddy">Supprimer</Button></td></tr>)}
+=======
+                {repertoire.map((piece, index) => <tr><td>Titre: {piece.titre}, Artiste: {piece.artiste}, Categorie: {piece.categorie} <br/>
+                <Link key={index} to={`/modifier/${index}`}><Button size="sm" active="true" variant="primary" >Modifier</Button></Link>
+                <nbsp/>
+                <Button size="sm" active="true" variant="primary" >Supprimer</Button></td></tr>)}
+>>>>>>> Stashed changes
             </Table>
         </>
     );
