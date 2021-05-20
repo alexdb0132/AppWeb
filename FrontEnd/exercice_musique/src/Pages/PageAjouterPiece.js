ab702878cd80804262369c6b2024b161b139e8c8
philiposAmigos <philant.daumas@gmail.com>
2021-05-19 22:32:04 -0400

    Implémentation des parties bootstrap et états de PageAjouterPiece.js

--- FrontEnd/exercice_musique/src/Pages/PageAjouterPiece.js
+++ FrontEnd/exercice_musique/src/Pages/PageAjouterPiece.js
@@ -1,32 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-
 function PageAjouterPiece(){
+    const [categories, setCategories] = useState([]);
+    const [piece, setPiece] = useState(
+        {
+            titre:"",
+            artiste: "",
+            categorie: ""
+        }
+    );
+
+    useEffect(() => {
+        const chercherDonnees = async () => {
+          const resultat = await fetch(`api/categories`);
+          const body = await resultat.json();
+          setCategories(body);
+        }
+
+        chercherDonnees();
+    }, []);
+
+    function ModifierTitre(p_titre){
+        const nouvellePiece = Object.assign({}, piece);
+
+        nouvellePiece.titre = p_titre;
+        setPiece(nouvellePiece );
+    }
+
+    function ModifierArtiste(p_artiste){
+        const nouvellePiece = Object.assign({}, piece);
+
+        nouvellePiece.artiste = p_artiste;
+        setPiece(nouvellePiece );
+    }
+
+    function ModifierCategorie(p_categorie){
+        const nouvellePiece = Object.assign({}, piece);
+
+        nouvellePiece.categorie = p_categorie;
+        setPiece(nouvellePiece );
+    }
+
     return (
         <>
-        <h1>Modifier une pièce existante</h1>
+        <h1>Ajouter une pièce</h1>
             <Form>
                 <Row>
                     <Col>
                         <Form.Group>
                             <Form.Label>Titre</Form.Label>
-                            <Form.Control type="text"/>
+                            <Form.Control type="text" onChange={(e) => ModifierTitre(e.target.value)}/>
                             <br/>
                             <Form.Label>Artiste</Form.Label>
-                            <Form.Control type="text"/>
+                            <Form.Control type="text" onChange={(e) => ModifierArtiste(e.target.value)}/>
                             <br/>
-                            <Form.Label>Categorie</Form.Label>
-                            <Form.Control type="text"/>
+                            <Form.Label>Catégorie</Form.Label>
+                            <Form.Control as="select" onChange={e => ModifierCategorie(e.target.value)}>
+                            {
+                                categories.map(categorie => 
+                                    <option value={`${categorie}`}>{categorie}</option>
+                                )
+                            }
+                            </Form.Control>
                         </Form.Group>
                         <br/>
                         <Row>
                             <Col>
-                            <Button>Modifier</Button>
+                            <Button>Ajouter</Button>
                             </Col>
                         </Row>
                     </Col>
--- FrontEnd/exercice_musique/src/Pages/PageModifierPiece.js
+++ FrontEnd/exercice_musique/src/Pages/PageModifierPiece.js
@@ -1,13 +1,10 @@
-import React, {useState, useEffect, setState} from 'react';
+import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {Link} from 'react-router-dom';
-
-
-
 
 function PageModifierPiece({ match }){
     const identifiant = match.params.id;
