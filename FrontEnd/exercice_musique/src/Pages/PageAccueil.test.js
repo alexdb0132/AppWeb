beb520ba88cf1a1431ca9866b5bc58f52dca101b
Alexandre <alexdb0132@gmail.com>
2021-05-26 16:17:48 -0400

    maj test page accueil

--- FrontEnd/exercice_musique/src/Pages/PageAccueil.test.js
+++ FrontEnd/exercice_musique/src/Pages/PageAccueil.test.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { render } from '@testing-library/react';
 import PageAccueil from "./PageAccueil"
+import '@testing-library/jest-dom/extend-expect'
 
 test('Affiche message bienvenu', () => {
   const {getByText} = render(<PageAccueil />);
   const interieurbalisep = getByText(/Affichez et gérez/);
-  expect(interieurbalisep).toBeInTheDocuments();
+  expect(interieurbalisep).toBeInTheDocument();
 });
