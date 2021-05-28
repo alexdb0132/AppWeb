692301c30fa2f87f23201cb6c0f9ddb0a9c3651c
bill <1996034@csfoy.ca>
2021-05-27 21:08:39 -0400

    ajout test organiserPiecesParCategorie

--- BackEnd/src/fonction.js
+++ BackEnd/src/fonction.js
@@ -12,3 +12,27 @@ export function chercherpiece(pieceRechercher ,pieces)
   });
   return pieceTrouvee;
 }
+
+export function organiserPiecesParcategorie()
+{
+  const piecesOrdreCategorie = pieces.slice();
+  piecesOrdreCategorie.sort((element1,element2) => 
+    (element1.categorie > element2.categorie) ? 1: -1);
+    return piecesOrdreCategorie;
+}
+export function AfficherTousCategorie()
+{
+  return categories;
+}
+
+export function AjouterPiece(piece)
+{
+  const {titre, artiste, categorie} = piece;
+
+  if (titre == null || artiste == null || categorie ==null ) {
+    reponse.send("il manque un champ obligatoire ");  
+  }
+  else{
+    pieces.push(piece)
+  }
+}
